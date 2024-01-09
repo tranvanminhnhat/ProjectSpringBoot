8920f7e1bc00b189d3881ac50c932530c1fd36d0
Minh Nhat <tranvanminhnhat99@gmail.com>
2024-01-09 12:11:28 +0700

    first commit

---
+++ note_project/src/main/java/com/nhat/note_project/NoteProjectApplication.java
@@ -0,0 +1,13 @@
+package com.nhat.note_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteProjectApplication.class, args);
+	}
+
+}
---
+++ note_project/src/test/java/com/nhat/note_project/NoteProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhat.note_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
